89580671ef6da9353412fe5a8a3f8f9a3cb2c485
haejin <haejin.yang91@gmail.com>
2023-10-11 19:01:41 +0900

    feat: 작업 5. 유저로부터 입력받은 데이터를 저장

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,4 +3,19 @@ console.log("popup.js loaded");
 document.getElementById("submit").addEventListener("click", (e) => {
   e.preventDefault();
   const userInput = document.getElementById("userInput").value;
+  document.getElementById("userInput").value = "";
+
+  chrome.storage.local.get(["key"]).then((result) => {
+    let value = [];
+    if (result.key) {
+      value = [...result.key];
+    } else {
+      value.push(userInput);
+    }
+
+    value.push(userInput);
+    chrome.storage.local.set({ key: value }).then(() => {
+      console.log("value is set : ", value);
+    });
+  });
 });
