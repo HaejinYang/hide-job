5364a2547880489113377dcdc4560eb43c40c430
haejin <haejin.yang91@gmail.com>
2023-10-11 18:53:58 +0900

    feat: 작업 4. 유저로부터 입력 데이터를 받음

---
+++ scripts/popup.js
@@ -0,0 +1,6 @@
+console.log("popup.js loaded");
+
+document.getElementById("submit").addEventListener("click", (e) => {
+  e.preventDefault();
+  const userInput = document.getElementById("userInput").value;
+});
