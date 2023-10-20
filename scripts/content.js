24bc54b0eec023dec180784e3eff7ccd0bef5c13
haejin <haejin.yang91@gmail.com>
2023-10-20 18:22:55 +0900

    refactor: 작업 12. content.js의 update를 update와 draw로 분리함

--- scripts/content.js
+++ scripts/content.js
@@ -1,60 +1,85 @@
 class Page {
+  #toBeHideList;
   #prevHideList;
+  #displayTargets;
+
   constructor() {
     this.#prevHideList = [];
+    this.#displayTargets = [];
+    this.#toBeHideList = [];
     const that = this;
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       const toBeHide = message.data;
-      that.#updateWithHide(toBeHide);
+      that.#updateInternel(toBeHide).then(() => {
+        that.draw();
+      });
     });
   }
 
-  update() {
-    chrome.storage.local.get(["key"]).then((result) => {
-      this.#prevHideList = [...result.key];
-      this.#updateWithHide(result.key);
+  run() {
+    this.update().then(() => {
+      this.draw();
     });
   }
 
-  #updateWithHide(toBeHide) {
-    chrome.storage.local.get(["key"]).then((result) => {
-      // 다시 진하게 할 대상 탐색
-      let display = [];
-      if (!this.#isPrevHideListEqual(result.key)) {
-        display = this.#prevHideList.filter(
-          (e) => !result.key.find((comp) => comp === e)
-        );
+  async update() {
+    const result = await chrome.storage.local.get(["key"]);
+    this.#prevHideList = [...result.key];
+    await this.#updateInternel(result.key);
+  }
+
+  draw() {
+    this.#display();
+    this.#hide();
+  }
+
+  #display() {
+    const posts = document.querySelectorAll(".post-list-corp");
+    for (let post of posts) {
+      const companyName = post.childNodes[1].getAttribute("title");
+      if (!this.#displayTargets.some((e) => e === companyName)) {
+        continue;
       }
 
-      // 갱신
-      const posts = document.querySelectorAll(".post-list-corp");
-      for (let post of posts) {
-        const companyName = post.childNodes[1].getAttribute("title");
-        if (display.some((e) => e === companyName)) {
-          const targets = post.parentNode.querySelectorAll("p, a");
-          for (let target of targets) {
-            if (target.style) {
-              target.style.color = "black";
-            }
-          }
-        } else {
-          if (!toBeHide.some((e) => e === companyName)) {
-            continue;
-          }
-
-          const targets =
-            post.parentNode.querySelectorAll("p, a, span, button");
-          for (let target of targets) {
-            if (target.style) {
-              target.style.backgroundColor = "white";
-              target.style.color = "white";
-            }
-          }
+      const targets = post.parentNode.querySelectorAll("p, a");
+      for (let target of targets) {
+        if (target.style) {
+          target.style.color = "black";
         }
       }
+    }
+  }
 
-      this.#prevHideList = [...result.key];
-    });
+  #hide() {
+    const posts = document.querySelectorAll(".post-list-corp");
+    for (let post of posts) {
+      const companyName = post.childNodes[1].getAttribute("title");
+      if (!this.#toBeHideList.some((e) => e === companyName)) {
+        continue;
+      }
+      console.log("hide", post);
+      const targets = post.parentNode.querySelectorAll("p, a, span, button");
+      for (let target of targets) {
+        if (target.style) {
+          target.style.backgroundColor = "white";
+          target.style.color = "white";
+        }
+      }
+    }
+  }
+
+  async #updateInternel(toBeHide) {
+    const result = await chrome.storage.local.get(["key"]);
+    // 그릴 대상
+    if (!this.#isPrevHideListEqual(result.key)) {
+      this.#displayTargets = this.#prevHideList.filter(
+        (e) => !result.key.find((comp) => comp === e)
+      );
+    }
+
+    // 숨길 대상
+    this.#toBeHideList = [...toBeHide];
+    this.#prevHideList = [...result.key];
   }
 
   #isPrevHideListEqual(toBeHide) {
@@ -67,4 +92,4 @@ class Page {
 }
 
 const page = new Page();
-page.update();
+page.run();
